aae3255715b5700fcde7dd1934915d3c635cb5e7
BintangDwitama <bintangdwitama160501@gmail.com>
2025-06-27 17:45:37 +0800

    separate gin handler

--- internal/transport/httpserver.go
+++ internal/transport/httpserver.go
@@ -1,15 +1,14 @@
 package transport
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/BintangDwitama/quest-board-be/internal/domain/handler"
+	"github.com/gin-gonic/gin"
+)
 
 func StartHTTPServer() {
 	router := gin.Default()
 
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Service Available",
-		})
-	})
+	router.GET("/ping", handler.Ping)
 
 	router.Run()
 }
